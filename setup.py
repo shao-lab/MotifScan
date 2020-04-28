3576e6193f660f32fc3e0d697ad2926f4994f0d3
haydensun <hayden_sun@hotmail.com>
2020-04-28 13:13:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,46 +1,79 @@
 import os
-from setuptools import setup, Extension, find_packages
-from motifscan import __version__
+import re
+import sys
 
-os.environ['CC'] = 'g++'
-os.environ['LDSHARED'] = 'g++'
+from setuptools import find_packages, setup, Extension
 
-INSTALL_REQUIRES = ['numpy',
-                    'scipy',
-                    'pandas>=0.17.0',
-                    'matplotlib'
-                    ]
+py_version = sys.version_info[:2]
+if py_version < (3, 6):
+    raise RuntimeError("MotifScan requires Python 3.6+ to install!")
 
-CLASSIFIERS = ['Development Status :: 3 - Alpha',
-               'Environment :: Console',
-               'Intended Audience :: Education',
-               'Intended Audience :: Science/Research',
-               'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-               'Operating System :: POSIX :: Linux',
-               'Operating System :: Unix',
-               'Programming Language :: Python',
-               'Programming Language :: Python :: 2',
-               'Programming Language :: Python :: 2.7',
-               'Topic :: Scientific/Engineering :: Bio-Informatics',
-               ]
+description = "A package for motif discovery and motif enrichment analysis"
 
-with open('README.rst', 'r') as fin:
+pkg_dir = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(pkg_dir, 'motifscan', '__init__.py')) as fin:
+    version = re.search(r"__version__ = '(.*?)'", fin.read()).group(1)
+
+with open(os.path.join(pkg_dir, 'README.rst')) as fin:
     long_description = fin.read()
 
-setup(name="motifscan",
-      version=__version__,
-      author="Jiawei Wang",
-      author_email="jerryeah@gmail.com",
-      maintainer="Hayden Sun",
-      maintainer_email="sunhongduo@picb.ac.cn",
-      url="http://bioinfo.sibs.ac.cn/shaolab/motifscan",
-      description="A motif discovery package developed by Shao lab in SIBS, CAS",
-      long_description=long_description,
-      license='GPLv3',
-      packages=find_packages(exclude=['*.tests']),
-      scripts=['bin/motifscan', 'bin/motifcompile', 'bin/genomecompile'],
-      ext_modules=[Extension(name='motifscan.score_c', sources=['motifscan/lib/score.cpp'], extra_link_args=["--shared"])],
-      install_requires=INSTALL_REQUIRES,
-      classifiers=CLASSIFIERS,
-      zip_safe=False
-      )
+install_requires = [
+    "numpy>=1.15",
+    "scipy>=1.0",
+    "requests",
+    "tqdm>=4.42.1",
+    "pysam>=0.15.0",
+    "matplotlib>=3.0.0"
+]
+
+extras_require = {
+    "test": ["pytest>=4.0.0",
+             "pytest-cov>=2.8.0"],
+    "docs": ["sphinx>=2.0.0",
+             "sphinx_rtd_theme"]
+}
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Environment :: Console",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: Unix",
+    "Operating System :: POSIX",
+    "Operating System :: MacOS",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Topic :: Scientific/Engineering :: Bio-Informatics"
+]
+
+ext_modules = [
+    Extension("motifscan.motif.score", ["motifscan/motif/score.c"])]
+
+setup(
+    name="motifscan",
+    version=version,
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url="https://github.com/shao-lab/MotifScan",
+    project_urls={
+        "Bug Tracker": "https://github.com/shao-lab/MotifScan/issues",
+        "Documentation": "https://motifscan.readthedocs.io",
+        "Source Code": "https://github.com/shao-lab/MotifScan",
+    },
+    author="Hayden Sun",
+    author_email="sunhongduo@picb.ac.cn",
+    license="BSD",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["motifscan=motifscan.cli.main:main"]},
+    python_requires=">=3.6",
+    install_requires=install_requires,
+    extras_require=extras_require,
+    ext_modules=ext_modules,
+    classifiers=classifiers,
+    zip_safe=False,
+)
