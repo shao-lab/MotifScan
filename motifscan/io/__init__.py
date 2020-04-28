3d411406753ffcfd442a52f3ca545b1e184416ec
haydensun <hayden_sun@hotmail.com>
2020-04-28 13:11:29 +0800

    Update IO module

---
+++ motifscan/io/__init__.py
@@ -0,0 +1,70 @@
+"""
+motifscan.io
+------------
+I/O functions for MotifScan.
+"""
+
+import os
+
+from motifscan.io.utils import replace_special_char
+
+
+def write_sites_table(output_dir, pwms, regions, motif_sites):
+    """Write the motif sites number/score summary table."""
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+    path_num = os.path.join(output_dir, 'motif_sites_number.xls')
+    path_score = os.path.join(output_dir, 'motif_sites_score.xls')
+    with open(path_num, 'w') as f_num, open(path_score, 'w') as f_score:
+        name_fields = '\t'.join([pwm.name for pwm in pwms])
+        f_num.write(f"chr\tstart\tend\t{name_fields}\n")
+        f_score.write(f"chr\tstart\tend\t{name_fields}\n")
+        for idx, region in enumerate(regions):
+            n_sites = []
+            scores = []
+            for sites in motif_sites:  # sites_by_motif
+                num = len(sites[idx])  # sites_by_region
+                n_sites.append(num)
+                if num == 0:
+                    scores.append('NA')
+                else:
+                    scores.append(max([site.score for site in sites[idx]]))
+            num_fields = '\t'.join(map(str, n_sites))
+            score_fields = '\t'.join(map(str, scores))
+            f_num.write(f"{region.chrom}\t{region.start + 1}\t{region.end}\t"
+                        f"{num_fields}\n")
+            f_score.write(f"{region.chrom}\t{region.start + 1}\t{region.end}\t"
+                          f"{score_fields}\n")
+
+
+def write_sites_bed(output_dir, pwms, regions, motif_sites):
+    """Write motif sites bed file(s)."""
+    output_dir = os.path.join(output_dir, 'motif_sites')
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+    for pwm, sites in zip(pwms, motif_sites):
+        name = replace_special_char(pwm.name)
+        path = os.path.join(output_dir, f"{name}_sites.bed")
+        with open(path, 'w') as f_out:
+            for idx, region in enumerate(regions):
+                for site in sites[idx]:
+                    f_out.write(f"{region.chrom}\t{site.start}\t"
+                                f"{site.start + pwm.length}\t.\t"
+                                f"{site.score}\t{site.strand}\n")
+
+
+def write_enrich_table(output_dir, enrichment_results):
+    """Write the motif enrichment results table."""
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+    path = os.path.join(output_dir, 'motif_enrichment.xls')
+    # sort by enriched p value and then fold change if p value ties
+    enrichment_results.sort(key=lambda x: (x.p_enriched, -x.fold_change))
+    with open(path, 'w') as f_out:
+        f_out.write("Motif\tNum_input_regions\tNum_control_regions\t"
+                    "Fold_change\tEnriched_P_value\tDepleted_P_value\t"
+                    "Corrected_P_value\n")
+        for res in enrichment_results:
+            f_out.write(f"{res.name}\t{res.n_input}\t{res.n_control}\t"
+                        f"{res.fold_change}\t{res.p_enriched}\t"
+                        f"{res.p_depleted}\t{res.p_corrected}\n")
---
+++ motifscan/io/utils.py
@@ -0,0 +1,111 @@
+"""
+motifscan.io.utils
+------------------
+
+Utilities for file operations.
+"""
+
+import re
+import gzip
+import shutil
+import tarfile
+import zipfile
+
+
+def replace_special_char(name):
+    return re.sub(r'[:\-.]', '_', name)
+
+
+def extract_zip(src, dst):
+    """Extract and merge zip compressed (.zip) file.
+
+    Parameters
+    ----------
+    src : str
+        Path of zip compressed file.
+    dst : str
+        Path to write the decompressed file.
+    """
+    with zipfile.ZipFile(src, 'r') as fin, open(dst, 'wb') as fout:
+        for member in fin.infolist():
+            shutil.copyfileobj(fin.open(member), fout)
+
+
+def extract_gzip(src, dst):
+    """Extract gzip compressed (.gz) file.
+
+    Parameters
+    ----------
+    src : str
+        Path of gzip compressed file.
+    dst : str
+        Path to write the decompressed file.
+    """
+    with gzip.open(src, 'rb') as fin, open(dst, 'wb') as fout:
+        shutil.copyfileobj(fin, fout)
+
+
+def extract_targz(src, dst):
+    """Extract and merge gzip compressed tar archive (.tar.gz) file.
+
+    Parameters
+    ----------
+    src : str
+        Path of gzip compressed tar archive file.
+    dst : str
+        Path to write the decompressed file.
+    """
+    with tarfile.open(src, 'r:gz') as fin, open(dst, 'wb') as fout:
+        for member in fin.getmembers():
+            if member.isfile():
+                shutil.copyfileobj(fin.extractfile(member), fout)
+
+
+def copy_file(src, dst):
+    """Copy file.
+
+    Parameters
+    ----------
+    src : str
+        Path of the file to copy from.
+    dst : str
+        Path to write the copied file.
+    """
+    with open(src, 'rb') as fin, open(dst, 'wb') as fout:
+        shutil.copyfileobj(fin, fout)
+
+
+def merge_files(src, dst):
+    """Merge(concatenate) files into one single file.
+
+    Parameters
+    ----------
+    src : str
+        Paths of the files to merge from.
+    dst : str
+        Path to write the merged file.
+    """
+    with open(dst, 'wb') as fout:
+        for tmp_file in src:
+            with open(tmp_file, 'rb') as fin:
+                shutil.copyfileobj(fin, fout)
+
+
+def merge_extracted_files(src, dst):
+    """Extract compressed files and merge them into a single file.
+
+    Parameters
+    ----------
+    src : str
+        Path of compressed files.
+    dst : str
+        Path to write the merged file.
+    """
+    if src.endswith('tar.gz'):
+        extract_targz(src, dst)
+    elif src.endswith('zip'):
+        extract_zip(src, dst)
+    elif src.endswith('gz'):
+        extract_gzip(src, dst)
+    else:
+        raise TypeError(f"unsupported compressed file: {src}")
