1f6442a12adda875a17aceabc5ea868ef2d8c00f
haydensun <hayden_sun@hotmail.com>
2020-05-05 16:27:12 +0800

    Ignore GitHub badge link check

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,3 +58,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+
+linkcheck_ignore = [
+    r"https://github.com/shao-lab/MotifScan/workflows/Python%20package/badge.svg"
+]
